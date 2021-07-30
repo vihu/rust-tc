5a15ad6788eebcb276c18358215799719b2f3448
Rahul Garg <rg@vihu.dev>
2021-07-30 12:45:53 -0700

    Add aggregate verify

--- src/sig.rs
+++ src/sig.rs
@@ -32,6 +32,29 @@ impl Signature {
 
         Ok(Signature(G2Affine::from(aggregate)))
     }
+
+    // TODO: Fix me
+    pub fn aggregate_verify<M: AsRef<[u8]>>(
+        public_keys: &[PublicKey],
+        msgs: &[M],
+        sig: Signature,
+    ) -> bool {
+        // check there are some keys and msgs
+        if public_keys.len() < 1 || msgs.len() < 1 {
+            return false;
+        }
+
+        let mut c1 = Gt::identity();
+
+        for i in 1..public_keys.len() {
+            let p = public_keys[i].0;
+            let q = hash_g2(msgs[i]);
+            let gt = pairing(&p, &q);
+            c1 = c1 * gt
+        }
+        let c2 = pairing(&G1Affine::generator(), &sig.0);
+        c1 == c2
+    }
 }
 
 #[cfg(test)]
