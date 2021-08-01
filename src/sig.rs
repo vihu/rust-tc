7ab8e72ce481261ff38c015b93f0a7b43fde7424
Rahul Garg <rg@vihu.dev>
2021-07-31 18:06:06 -0700

    Switch to my fork for testing agg sig

--- src/sig.rs
+++ src/sig.rs
@@ -1,6 +1,9 @@
 use crate::{pk::PublicKey, util::hash_g2};
 use anyhow::{bail, Result};
-use bls12_381::{pairing, G1Affine, G2Affine, G2Projective, Gt, Scalar};
+use bls12_381::{
+    multi_miller_loop, pairing, G1Affine, G2Affine, G2Prepared, G2Projective, Gt, MillerLoopResult,
+    Scalar,
+};
 use std::ops::{AddAssign, Mul};
 
 const SIGSIZE: usize = 96;
@@ -12,80 +15,115 @@ impl Signature {
     pub fn validate(&self) -> bool {
         self.0.to_compressed().len() == SIGSIZE
     }
+}
 
-    pub fn aggregate(sigs: &[Signature]) -> Result<Signature> {
-        let agg = &sigs[0];
+pub fn aggregate(sigs: &[Signature]) -> Result<Signature> {
+    let agg = &sigs[0];
 
-        if !agg.validate() {
-            bail!("Cannot validate signature {:?}", agg)
-        }
-
-        let mut aggregate = G2Projective::from(sigs[0].0);
-
-        for i in 2..sigs.len() {
-            let next = &sigs[i];
-            if !next.validate() {
-                bail!("Cannot validate signature {:?}", next)
-            }
-            aggregate.add_assign(&next.0)
-        }
-
-        Ok(Signature(G2Affine::from(aggregate)))
+    if !agg.validate() {
+        bail!("Cannot validate signature {:?}", agg)
     }
 
-    // TODO: Fix me
-    // pub fn core_aggregate_verify<M: AsRef<[u8]>>(
-    //     public_keys: &[PublicKey],
-    //     msgs: &[M],
-    //     sig: Signature,
-    // ) -> Result<bool> {
-    //     if !sig.validate() {
-    //         bail!("Invalid signature: {:?}", sig);
-    //     }
+    let mut aggregate = G2Projective::from(sigs[0].0);
 
-    //     // check there are some keys and msgs
-    //     if public_keys.len() < 1 || msgs.len() < 1 {
-    //         bail!("Invalid public_keys or msgs length");
-    //     }
+    for i in 1..sigs.len() {
+        let next = &sigs[i];
+        if !next.validate() {
+            bail!("Cannot validate signature {:?}", next)
+        }
+        aggregate.add_assign(&next.0)
+    }
 
-    //     // check that the lengths match
-    //     if public_keys.len() != msgs.len() {
-    //         bail!("Invalid public_keys and msgs length don't match!")
-    //     }
+    Ok(Signature(G2Affine::from(aggregate)))
+}
 
-    //     let mut c1 = Gt::identity();
+pub fn core_aggregate_verify(
+    signature: &Signature,
+    hashes: &[G2Affine],
+    public_keys: &[PublicKey],
+) -> Result<bool> {
+    // Either public_keys or hashes is empty, bail
+    if hashes.is_empty() || public_keys.is_empty() {
+        bail!(
+            "Either hashes {:?} or public_keys {:?} is empty",
+            hashes,
+            public_keys
+        )
+    }
 
-    //     for i in 1..public_keys.len() {
-    //         let pk = public_keys[i];
-    //         if !pk.validate() {
-    //             bail!("Invalid public_key: {:?}", pk)
-    //         }
-    //         let p = public_keys[i].0;
-    //         let q = hash_g2(msgs[i]);
-    //         let gt = pairing(&p, &q);
-    //         c1 = gt * c1;
-    //     }
-    //     let c2 = pairing(&G1Affine::generator(), &sig.0);
-    //     Ok(c1 == c2)
-    // }
+    // Bail if public_keys don't line up with hashes
+    let num_hashes = hashes.len();
+    if num_hashes != public_keys.len() {
+        bail!("Length mismatch for public_keys and hashes!")
+    }
+
+    // Bail if non-unique hashes found!
+    for i in 0..(num_hashes - 1) {
+        for j in (i + 1)..num_hashes {
+            let a = hashes[i];
+            let b = hashes[j];
+            if a == b {
+                bail!("Non-unique hashes found! {:?} {:?}", a, b)
+            }
+        }
+    }
+
+    let c1: Gt = public_keys
+        .iter()
+        .zip(hashes.iter())
+        .map(|(pk, h)| {
+            let pk = pk.0;
+            let h = G2Prepared::from(*h);
+            multi_miller_loop(&[(&pk, &h)])
+        })
+        .fold(MillerLoopResult::default(), |mut acc, cur| {
+            acc = acc.mul(&cur);
+            acc
+        })
+        .final_exponentiation();
+
+    let c2: Gt = pairing(&G1Affine::generator(), &signature.0);
+
+    Ok(c1 == c2)
+}
+
+/// Verifies that the signature is the actual aggregated signature of messages - pubkeys.
+/// Calculated by `e(g1, signature) == \prod_{i = 0}^n e(pk_i, hash_i)`.
+pub fn verify_messages(
+    signature: &Signature,
+    messages: &[&[u8]],
+    public_keys: &[PublicKey],
+) -> Result<bool> {
+    let hashes: Vec<_> = messages.iter().map(|msg| hash_g2(msg)).collect();
+
+    core_aggregate_verify(signature, &hashes, public_keys)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Signature;
+    use super::*;
     use crate::sk::SecretKey;
-    // use bls12_381::{Gt, Scalar};
-    // use ff::Field;
-    // use rand::thread_rng;
-    // use std::ops::Mul;
 
     #[test]
     fn aggregate_sig() {
-        let sigs = (0u32..10)
-            .map(|_| SecretKey::new().sign("sign me"))
-            .collect::<Vec<Signature>>();
-        let agg_sig = Signature::aggregate(&sigs);
-        println!("agg_sig: {:?}", agg_sig)
+        let sk1 = SecretKey::new();
+        let pk1 = sk1.public_key();
+        let sk2 = SecretKey::new();
+        let pk2 = sk2.public_key();
+
+        let msg1 = b"Rip and tear";
+        let msg2 = b"till is done";
+
+        let sig1 = sk1.sign(msg1);
+        let sig2 = sk2.sign(msg2);
+
+        if let Ok(agg_sig) = aggregate(&[sig1, sig2]) {
+            if let Ok(res) = verify_messages(&agg_sig, &[msg1, msg2], &[pk1, pk2]) {
+                assert!(res)
+            }
+        } else {
+            assert!(false)
+        }
     }
 
     #[test]
